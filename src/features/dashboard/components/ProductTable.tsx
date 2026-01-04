9cbf73c31fb67e1f09aa8554c86460c758a4295c
aditya03-fend <adityaportfolio3@gmail.com>
2026-01-04 13:14:32 +0700

    chore: squash merge feature/dashboard into main

--- src/features/dashboard/components/ProductTable.tsx
+++ src/features/dashboard/components/ProductTable.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Table,
@@ -7,20 +7,16 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { Product } from "@/features/dashboard/types/dashboard.types"
+} from "@/components/ui/table";
+import { Product } from "@/features/dashboard/types/dashboard.types";
 
 type Props = {
-  products: Product[]
-}
+  products: Product[];
+};
 
 export function ProductTable({ products }: Props) {
   if (products.length === 0) {
-    return (
-      <p className="text-sm text-muted-foreground">
-        No products found.
-      </p>
-    )
+    return <p className="text-sm text-muted-foreground">No products found.</p>;
   }
 
   return (
@@ -30,24 +26,32 @@ export function ProductTable({ products }: Props) {
           <TableRow>
             <TableHead>Name</TableHead>
             <TableHead>Category</TableHead>
-            <TableHead className="text-right">Price</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Discount</TableHead>
+            <TableHead>Rating</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead className="text-right">Brand</TableHead>
           </TableRow>
         </TableHeader>
 
         <TableBody>
           {products.map((product) => (
             <TableRow key={product.id}>
-              <TableCell className="font-medium">
-                {product.title}
-              </TableCell>
+              <TableCell className="font-medium">{product.title}</TableCell>
               <TableCell>{product.category}</TableCell>
-              <TableCell className="text-right">
-                ${product.price}
-              </TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.discountPercentage}</TableCell>
+              <TableCell>{product.rating}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              {product.brand ? (
+                <TableCell className="text-right">{product.brand}</TableCell>
+              ) : (
+                <TableCell className="text-right">No Brand</TableCell>
+              )}
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </div>
-  )
+  );
 }
